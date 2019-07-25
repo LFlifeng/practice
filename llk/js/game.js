c023a70b516b703c705dd14e82b224b642d731cd
LFlfeng <2238561742@qq.com>
2019-07-25 17:27:27 +0800

    ..

--- llk/js/game.js
+++ llk/js/game.js
@@ -1,4 +1,4 @@
-var Game = (function(){
+var Game = (() => {
     var Row = config.row + 2;
     var COL = config.col + 2;
     var itemCount = config.row * config.col;
@@ -8,7 +8,7 @@ var Game = (function(){
     };
     var timeCooldown = 60;
     var helpData = [];
-    var Game = function(){
+    var Game = () =>{
 
     };
     Game.prototype = {
@@ -68,7 +68,10 @@ var Game = (function(){
                         var x = random(1,col);
                         var y = random(1,row);
                         var item = data.cell[y][x].val = i;
-                        break;
+                        if(item.val === null){
+                            data.cell[y][x].val = i;
+                            break;
+                        }
                     }
                 }
             }
@@ -131,7 +134,7 @@ var Game = (function(){
             var success = false;
             var min = Math.min.call(null,before,after);
             var max = Math.max.call(null,before,after);
-            var called = function(dir){
+            var called = (dir) => {
                 var i = min;
                 var num = dir === 'x' ? COL : 1;
                 for(var i=0;i+=num;i<=max){
@@ -170,7 +173,7 @@ var Game = (function(){
             var success = false;
             var pos = [];
             var corners = this.getCorner(bofore,after);
-            corners.forEach(function(el){
+            corners.forEach((el) => {
                 if(!_this.isEmpty(_this.getItem(el)) || success){
                     return;
                 }
@@ -178,7 +181,7 @@ var Game = (function(){
                     _this.connectable(before,el),
                     _this.connectable(el,after),
                 ];
-                var ok = _status.every(function(status){
+                var ok = _status.every((status) => {
                     return status.success;
                 });
                 if(ok){
@@ -217,7 +220,7 @@ var Game = (function(){
                 }
             }
             return {
-                success: successs,
+                success: success,
                 pos: pos,
             }
         },
@@ -243,7 +246,114 @@ var Game = (function(){
             return status;
         },
         judge(before,after){
-            
+            var _this = this;
+            var status = this.isConnectable(before,after);
+            if(status && status.success){
+                if(status && status.success){
+                    status.pos.unshift(before);
+                    status.pos.push(after);
+                    this.view.showLine(status.pos,(){
+                        _this.removeItem(before,after);
+                    });
+                }else{
+                    this.removeItem(before,after);
+                }
+                return true;
+            }else{
+                return false;
+            }
+        },
+        isOutside(index){
+            var pos = this.indexToPos(index);
+            return (
+                pos.x < 0 || pos.y < 0 || pos.x > COL-1 || pos.y > Row-1
+            );
+        },
+        isLimit(index){
+            var pos = this.indexToPos(index);
+            return (
+                pos.x === 0 || pos.y === 0 || pos.x === COL-1 || pos.y === Row-1
+            );
+        },
+        getItem(index){
+            if(this.isOutside(index)){
+                return {};
+            }
+            var pos = this.indexToPos(index);
+            return data.cell[pos.y][pos.x];
+        },
+        winning(){
+            setTimeout(() => {
+                var str = "已经完成，确定再来一局吗？";
+                alert(str);
+                location.reload();
+            },50);
+        },
+        over(){
+            data.time = config.time;
+            var str = "失败！确定再来一次吗？";
+            alert(str);
+            location.reload();
+        },
+        checkWinning(){
+            if(itemCount ===0 ){
+                this.winning();
+            }else{
+                this.checkDeadlock();
+            }
+        },
+        checkDeadlock(){
+            log(1);
+            var count = config.objectCount;
+            var cell = reduceDimension(data.cell);
+            var filter = (i) => {
+                return cell.filter((el) => {
+                    return el.val === i;
+                });
+            };
+            for(var i=0;i<count;i++){
+                var result = filter(i);
+                var len = result.length;
+                for(var j=0;j<len;j++){
+                    var el = result[j].index;
+                    for(var k=0;k<len;k++){
+                        var status = this.isConnectable(el,result[k].index);
+                        if(status && status.success) {
+                            helpData = [el,result[k].index];
+                            return;
+                        }
+                    }
+                }
+            }
+            this.randomReset();
+        },
+        randomReset(){
+            var _this = this;
+            var row = config.row;
+            var col = config.col;
+            var cell = (() => {
+                return reduceDimension(data.cell).filter((el) => {
+                    return el.val !== null;
+                });
+            })();
+            this.initCell();
+            cell.forEach((el) => {
+                while(true){
+                    var x = random(1,col);
+                    var y = random(1,row);
+                    var item = data.cell[y][x];
+                    if(item.val === null){
+                        data.cell[y][x] = {
+                            val: el.val,
+                            index: _this.posToIndex({x: x,y: y}),
+                        }
+                        break;
+                    }
+                }
+            });
+            this.view.init(this,data);
+            this.checkDeadlock();
         }
     }
-})
+    return Game;
+})();
