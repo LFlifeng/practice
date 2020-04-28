aea05dc861a1f9faa46b6d42721615491761519f
LFlfeng <2238561742@qq.com>
2020-04-28 13:38:36 +0800

    年会抽奖

---
+++ nianhuichoujiang/js/member.js
@@ -0,0 +1,522 @@
+var member = [
+  {
+    "name": "韩志鹏",
+    "path": "./images/0.png",
+    "count": 1
+  },
+
+  {
+    "name": "王雨杰",
+    "path": "./images/1.png",
+    "count": 1
+  },
+
+  {
+    "name": "廖兰秀",
+    "path": "./images/2.png",
+    "count": 1
+  },
+
+  {
+    "name": "陈金伟",
+    "path": "./images/3.png",
+    "count": 1
+  },
+
+  {
+    "name": "刘作胤",
+    "path": "./images/4.png",
+    "count": 1
+  },
+
+  {
+    "name": "宋思彤",
+    "path": "./images/5.png",
+    "count": 1
+  },
+
+  {
+    "name": "樊明超",
+    "path": "./images/6.png",
+    "count": 1
+  },
+
+  {
+    "name": "杨丹",
+    "path": "./images/7.png",
+    "count": 1
+  },
+
+  {
+    "name": "炼智良",
+    "path": "./images/8.png",
+    "count": 1
+  },
+
+  {
+    "name": "马涛",
+    "path": "./images/9.png",
+    "count": 1
+  },
+
+  {
+    "name": "龚天文",
+    "path": "./images/10.png",
+    "count": 1
+  },
+
+  {
+    "name": "王梦浩",
+    "path": "./images/11.png",
+    "count": 1
+  },
+
+  {
+    "name": "肖岩",
+    "path": "./images/12.png",
+    "count": 1
+  },
+
+  {
+    "name": "赵路群",
+    "path": "./images/13.png",
+    "count": 1
+  },
+
+  {
+    "name": "张桢",
+    "path": "./images/14.png",
+    "count": 1
+  },
+
+  {
+    "name": "任立国",
+    "path": "./images/15.png",
+    "count": 1
+  },
+
+  {
+    "name": "苏兴隆",
+    "path": "./images/16.png",
+    "count": 1
+  },
+
+  {
+    "name": "弓垒",
+    "path": "./images/17.png",
+    "count": 1
+  },
+
+  {
+    "name": "孟祥东",
+    "path": "./images/18.png",
+    "count": 1
+  },
+
+  {
+    "name": "黄博珍",
+    "path": "./images/19.png",
+    "count": 1
+  },
+
+  {
+    "name": "董申",
+    "path": "./images/20.png",
+    "count": 1
+  },
+
+  {
+    "name": "李伟",
+    "path": "./images/21.png",
+    "count": 1
+  },
+
+  {
+    "name": "陈晓珍",
+    "path": "./images/22.png",
+    "count": 1
+  },
+
+  {
+    "name": "齐玉雪",
+    "path": "./images/23.png",
+    "count": 1
+  },
+
+  {
+    "name": "陈龙1",
+    "path": "./images/24.png",
+    "count": 1
+  },
+
+  {
+    "name": "郝超永",
+    "path": "./images/25.png",
+    "count": 1
+  },
+
+  {
+    "name": "刘畅",
+    "path": "./images/26.png",
+    "count": 1
+  },
+
+  {
+    "name": "尚飒",
+    "path": "./images/27.png",
+    "count": 1
+  },
+
+  {
+    "name": "张伟",
+    "path": "./images/28.png",
+    "count": 1
+  },
+
+  {
+    "name": "黄永蕊",
+    "path": "./images/29.png",
+    "count": 1
+  },
+
+  {
+    "name": "张建梅",
+    "path": "./images/30.png",
+    "count": 1
+  },
+
+  {
+    "name": "刘宏伟",
+    "path": "./images/31.png",
+    "count": 1
+  },
+
+  {
+    "name": "洪敏",
+    "path": "./images/32.png",
+    "count": 1
+  },
+
+  {
+    "name": "李健",
+    "path": "./images/33.png",
+    "count": 1
+  },
+
+  {
+    "name": "张志亮",
+    "path": "./images/34.png",
+    "count": 1
+  },
+
+  {
+    "name": "李健涛",
+    "path": "./images/35.png",
+    "count": 1
+  },
+
+  {
+    "name": "郄建飞",
+    "path": "./images/36.png",
+    "count": 1
+  },
+
+  {
+    "name": "李海超",
+    "path": "./images/37.png",
+    "count": 1
+  },
+
+  {
+    "name": "赵海波",
+    "path": "./images/38.png",
+    "count": 1
+  },
+
+  {
+    "name": "徐新锋",
+    "path": "./images/39.png",
+    "count": 1
+  },
+
+  {
+    "name": "陈明波",
+    "path": "./images/40.png",
+    "count": 1
+  },
+
+  {
+    "name": "杜太广",
+    "path": "./images/41.png",
+    "count": 1
+  },
+
+  {
+    "name": "陈龙",
+    "path": "./images/42.png",
+    "count": 1
+  },
+
+  {
+    "name": "李国强",
+    "path": "./images/43.png",
+    "count": 1
+  },
+
+  {
+    "name": "巫珊珊",
+    "path": "./images/44.png",
+    "count": 1
+  },
+
+  {
+    "name": "雷少辉",
+    "path": "./images/45.png",
+    "count": 1
+  },
+
+  {
+    "name": "李向杰",
+    "path": "./images/46.png",
+    "count": 1
+  },
+
+  {
+    "name": "朱世妍",
+    "path": "./images/47.png",
+    "count": 1
+  },
+
+  {
+    "name": "陈晓俊",
+    "path": "./images/48.png",
+    "count": 1
+  },
+
+  {
+    "name": "潘小雪",
+    "path": "./images/49.png",
+    "count": 1
+  },
+
+  {
+    "name": "华伟",
+    "path": "./images/50.png",
+    "count": 1
+  },
+
+  {
+    "name": "张旭红",
+    "path": "./images/51.png",
+    "count": 1
+  },
+
+  {
+    "name": "乔亚伟",
+    "path": "./images/52.png",
+    "count": 1
+  },
+
+  {
+    "name": "刘松霞",
+    "path": "./images/53.png",
+    "count": 1
+  },
+
+  {
+    "name": "尤志艳",
+    "path": "./images/54.png",
+    "count": 1
+  },
+
+  {
+    "name": "孟美娜",
+    "path": "./images/55.png",
+    "count": 1
+  },
+
+  {
+    "name": "刘文新",
+    "path": "./images/56.png",
+    "count": 1
+  },
+
+  {
+    "name": "杨永强",
+    "path": "./images/57.png",
+    "count": 1
+  },
+
+  {
+    "name": "申迎起",
+    "path": "./images/58.png",
+    "count": 1
+  },
+
+  {
+    "name": "柴盈源",
+    "path": "./images/59.png",
+    "count": 1
+  },
+
+  {
+    "name": "余冰",
+    "path": "./images/60.png",
+    "count": 1
+  },
+
+  {
+    "name": "王久通",
+    "path": "./images/61.png",
+    "count": 1
+  },
+
+  {
+    "name": "鲍立柱",
+    "path": "./images/62.png",
+    "count": 1
+  },
+
+  {
+    "name": "齐月月",
+    "path": "./images/63.png",
+    "count": 1
+  },
+
+  {
+    "name": "李敏",
+    "path": "./images/64.png",
+    "count": 1
+  },
+
+  {
+    "name": "齐亚州",
+    "path": "./images/65.png",
+    "count": 1
+  },
+
+  {
+    "name": "李晓辉",
+    "path": "./images/66.png",
+    "count": 1
+  },
+
+  {
+    "name": "庞欢",
+    "path": "./images/67.png",
+    "count": 1
+  },
+
+  {
+    "name": "吴磊",
+    "path": "./images/68.png",
+    "count": 1
+  },
+
+  {
+    "name": "袁春光",
+    "path": "./images/69.png",
+    "count": 1
+  },
+
+  {
+    "name": "穆中彪",
+    "path": "./images/70.png",
+    "count": 1
+  },
+
+  {
+    "name": "高忆",
+    "path": "./images/71.png",
+    "count": 1
+  },
+
+  {
+    "name": "张妍馨",
+    "path": "./images/72.png",
+    "count": 1
+  },
+
+  {
+    "name": "孙良良",
+    "path": "./images/73.png",
+    "count": 1
+  },
+
+  {
+    "name": "高志鹏",
+    "path": "./images/74.png",
+    "count": 1
+  },
+
+  {
+    "name": "杨成明",
+    "path": "./images/75.png",
+    "count": 1
+  },
+
+  {
+    "name": "段振荣",
+    "path": "./images/76.png",
+    "count": 1
+  },
+
+  {
+    "name": "郑贺",
+    "path": "./images/77.png",
+    "count": 1
+  },
+
+  {
+    "name": "王家鹏",
+    "path": "./images/78.png",
+    "count": 1
+  },
+
+  {
+    "name": "林琨鹏",
+    "path": "./images/79.png",
+    "count": 1
+  },
+
+  {
+    "name": "王芳文",
+    "path": "./images/80.png",
+    "count": 1
+  },
+
+  {
+    "name": "姜帆",
+    "path": "./images/81.png",
+    "count": 1
+  },
+
+  {
+    "name": "邵玉丽",
+    "path": "./images/82.png",
+    "count": 1
+  },
+  {
+    "name": "赵清宇",
+    "path": "./images/83.png",
+  "count": 1
+  },
+
+{
+    "name": "邢二梅",
+    "path": "./images/84.png",
+  "count": 1
+  },
+
+{
+    "name": "高佳伟",
+    "path": "./images/85.png",
+  "count": 1
+  },
+
+{
+    "name": "刘欢欢",
+    "path": "./images/86.png",
+  "count": 1
+  },
+]
